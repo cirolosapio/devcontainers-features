e06ef61df5c53014c653601114adf9d3e140418c
cirolosapio <lscorionjs@gmail.com>
2023-09-25 18:47:03 +0200

    updates

--- src/alpine-docker/install.sh
+++ src/alpine-docker/install.sh
@@ -6,6 +6,7 @@ echo "Activating feature 'alpine-docker'"
 
 apk add --no-cache docker openrc
 
+rc-update add docker boot
 
 if [[ $INSTALLDOCKERCOMPOSE == "true" ]]; then
     apk add --no-cache docker-compose
