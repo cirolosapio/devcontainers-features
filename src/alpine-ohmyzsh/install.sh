b937bea7f420e2bfaaecaf2ed375f6ba77d0359a
cirolosapio <lscorionjs@gmail.com>
2023-10-02 18:27:39 +0200

    update

--- src/alpine-ohmyzsh/install.sh
+++ src/alpine-ohmyzsh/install.sh
@@ -8,17 +8,16 @@ apk --no-cache add git curl zsh
 
 CURRENT_USER=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
 echo "Acting as $CURRENT_USER"
-su $CURRENT_USER
 
-curl -s https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh -s
+su -c "curl -s https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh -s" $CURRENT_USER
 
-git clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-sed -i 's/plugins=(git)/plugins=(\n  git\n  sudo\n  k\n  zsh-autosuggestions\n  zsh-syntax-highlighting\n)/' ~/.zshrc
+git clone https://github.com/supercrabtree/k $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/k
+git clone https://github.com/zsh-users/zsh-autosuggestions $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sed -i 's/plugins=(git)/plugins=(\n  git\n  sudo\n  k\n  zsh-autosuggestions\n  zsh-syntax-highlighting\n)/' $_CONTAINER_USER_HOME/.zshrc
 
 if [[ $INITSTARSHIP == "true" ]]; then
-  echo $'\neval "$(starship init zsh)"' >> ~/.zshrc
+  echo $'\neval "$(starship init zsh)"' >> $_CONTAINER_USER_HOME/.zshrc
 fi
 
 echo 'Done!'
