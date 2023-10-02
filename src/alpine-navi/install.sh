c7b2c7778cfb5f3f1a634156e82173733d560c15
cirolosapio <lscorionjs@gmail.com>
2023-10-02 14:32:38 +0200

    add navi feature

---
+++ src/alpine-navi/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+echo "Activating feature 'alpine-navi'"
+
+apk --no-cache add skim
+apk --no-cache add navi --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing
+
+# if $INITZSH is true, then add navi to zshrc
+if [[ $INITZSH == "true" ]]; then
+  echo $'\neval "$(navi widget zsh)"' >> $_CONTAINER_USER_HOME/.zshrc
+fi
+
+echo 'Done!'
