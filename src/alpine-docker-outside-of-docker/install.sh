0c8fed4d8be57389cf91945c29cb306a9a10c5b3
cirolosapio <lscorionjs@gmail.com>
2023-10-03 21:41:25 +0200

    updates

--- src/alpine-docker-outside-of-docker/install.sh
+++ src/alpine-docker-outside-of-docker/install.sh
@@ -8,19 +8,24 @@ apk add --no-cache docker
 
 CURRENT_USER=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
 echo "CURRENT_USER is $CURRENT_USER"
-echo "HOST_DOCKER_GID is $HOSTDOCKERGID"
 
-HOST_DOCKER_GROUP=$(getent group $HOSTDOCKERGID | cut -d: -f1)
-
-if [[ -z $HOST_DOCKER_GROUP ]]; then
-    echo "HOST_DOCKER_GROUP is empty, creating new group 'host-docker'"
-    addgroup -g $HOSTDOCKERGID host-docker
-    HOST_DOCKER_GROUP=host-docker
+if [[ -z $CURRENT_USER ]]; then
+    echo "CURRENT_USER is empty, using 'root'"
 else
-    echo "HOST_DOCKER_GROUP is already used, using existing group '$HOST_DOCKER_GROUP'"
-fi
+    echo "HOST_DOCKER_GID is $HOSTDOCKERGID"
 
-addgroup $CURRENT_USER $HOST_DOCKER_GROUP
+    HOST_DOCKER_GROUP=$(getent group $HOSTDOCKERGID | cut -d: -f1)
+
+    if [[ -z $HOST_DOCKER_GROUP ]]; then
+        echo "HOST_DOCKER_GROUP is empty, creating new group 'host-docker'"
+        addgroup -g $HOSTDOCKERGID host-docker
+        HOST_DOCKER_GROUP=host-docker
+    else
+        echo "HOST_DOCKER_GROUP is already used, using existing group '$HOST_DOCKER_GROUP'"
+    fi
+
+    addgroup $CURRENT_USER $HOST_DOCKER_GROUP
+fi
 
 if [[ $INSTALLDOCKERCOMPOSE == "true" ]]; then
     apk add --no-cache docker-compose
--- src/alpine-ohmyzsh/install.sh
+++ src/alpine-ohmyzsh/install.sh
@@ -9,6 +9,10 @@ apk --no-cache add git curl zsh
 CURRENT_USER=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
 su -c "curl -s https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh -s" $CURRENT_USER
 
+if [[ $CURRENT_USER != "root" ]] && [[ $_CONTAINER_USER_HOME == "/root" ]]; then
+  _CONTAINER_USER_HOME="/home/$CURRENT_USER"
+fi
+
 git clone https://github.com/supercrabtree/k $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/k
 git clone https://github.com/zsh-users/zsh-autosuggestions $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
