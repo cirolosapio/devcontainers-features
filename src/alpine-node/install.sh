9bf5aced69ca67bc7ed3a44409e33a8c66e7fc33
cirolosapio <lscorionjs@gmail.com>
2023-10-03 12:02:39 +0200

    updates

--- src/alpine-node/install.sh
+++ src/alpine-node/install.sh
@@ -6,4 +6,17 @@ echo "Activating feature 'alpine-node'"
 
 apk --no-cache add nodejs npm
 
+if [[ $COREPACK == "true" ]]; then
+  npm i -g corepack
+  corepack enable
+fi
+
+if [[ $ANTFUNI == "true" ]]; then
+  npm i -g @antfu/ni
+fi
+
+if [[ $TSNODE == "true" ]]; then
+  npm install -g typescript ts-node
+fi
+
 echo 'Done!'
