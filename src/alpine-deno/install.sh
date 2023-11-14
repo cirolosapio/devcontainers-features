00dd9ec0b1de12eb67cb77d8d2334b3f9e5575bc
cirolosapio <lscorionjs@gmail.com>
2023-11-14 10:59:21 +0100

    update

--- src/alpine-deno/install.sh
+++ src/alpine-deno/install.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Activating feature 'alpine-deno'"
 
-apk --no-cache add deno --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community
+apk --no-cache add deno --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community --repository=http://dl-cdn.alpinelinux.org/alpine/edge/main
 
 if [[ $INITZSH == "true" ]]; then
   apk --no-cache add deno-zsh-completion --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community
