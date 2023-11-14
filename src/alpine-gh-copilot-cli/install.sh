69fc051ace6e3f353880b4ff8ebc0191d257c844
cirolosapio <lscorionjs@gmail.com>
2023-11-14 21:36:42 +0100

    fix gh-copilot-cli alias

--- src/alpine-gh-copilot-cli/install.sh
+++ src/alpine-gh-copilot-cli/install.sh
@@ -8,7 +8,14 @@ apk --no-cache add npm
 npm i -g @githubnext/github-copilot-cli
 
 if [[ $INITALIAS == "true" ]]; then
-  echo 'eval "$(github-copilot-cli alias -- "$0")"' >> $_CONTAINER_USER_HOME/.zshrc
+  CURRENT_USER=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
+  echo "CURRENT_USER is $CURRENT_USER"
+  
+  if [[ -z $CURRENT_USER ]]; then
+    echo 'eval "$(github-copilot-cli alias -- "$0")"' >> $_CONTAINER_USER_HOME/.zshrc
+  else 
+    su -c "echo 'eval "$(github-copilot-cli alias -- "$0")"' >> $_CONTAINER_USER_HOME/.zshrc" $CURRENT_USER
+  fi
 fi
 
 echo 'Done!'
