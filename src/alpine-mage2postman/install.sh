c5d9d5350e512da42b917bb07319edd3e5fa39ae
cirolosapio <lscorionjs@gmail.com>
2024-03-18 16:43:42 +0100

    update alpine-mage2postman

--- src/alpine-mage2postman/install.sh
+++ src/alpine-mage2postman/install.sh
@@ -6,7 +6,7 @@ echo "Activating feature 'alpine-mage2postman'"
 
 apk --no-cache add zstd libffi icu zlib-ng sqlite-libs
 
-wget -q https://github.com/cirolosapio/mage2postman/releases/download/latest/mage2postman-alpine -O /usr/local/bin/mage2postman
+wget -q https://github.com/cirolosapio/mage2postman/releases/download/v0.0.2/mage2postman-alpine -O /usr/local/bin/mage2postman
 chmod +x /usr/local/bin/mage2postman
 
 echo 'Done!'
