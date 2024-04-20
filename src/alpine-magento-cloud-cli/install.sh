d1360eb0f0d39c62cbcb641189809e1b2d4f2c2e
cirolosapio <lscorionjs@gmail.com>
2024-04-20 13:43:16 +0200

    chore: Update devcontainer feature for Alpine Magento Cloud CLI

--- src/alpine-magento-cloud-cli/install.sh
+++ src/alpine-magento-cloud-cli/install.sh
@@ -4,14 +4,19 @@ set -e
 
 echo "Activating feature 'alpine-magento-cloud-cli'"
 
-apk --no-cache add curl
+apk --no-cache add curl git
 
 CURRENT_USER=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
 
 CMD="cd $_CONTAINER_USER_HOME && curl -sS https://accounts.magento.cloud/cli/installer | php"
 
-if command -v zsh &> /dev/null; then
+if [ "$SHELLTYPE" = "zsh" ]; then
+    apk --no-cache add zsh
+    sed -i "s|:/bin/ash|:/bin/zsh|g" /etc/passwd
     CMD="$CMD -- --shell-type zsh"
+else
+    apk --no-cache add bash
+    CMD="$CMD -- --shell-type bash"
 fi
 
 su -c "$CMD" $CURRENT_USER
