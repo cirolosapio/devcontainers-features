dcb94374bf62fd4d1a37f33dafa0b4e97ab28d55
cirolosapio <lscorionjs@gmail.com>
2024-04-20 15:32:15 +0200

    chore: Remove unnecessary echo statements in install.sh

--- src/alpine-gh-copilot-cli/install.sh
+++ src/alpine-gh-copilot-cli/install.sh
@@ -9,8 +9,6 @@ npm i -g @githubnext/github-copilot-cli
 
 if [[ $INITALIAS == "true" ]]; then
   CURRENT_USER=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
-  echo "CURRENT_USER is $CURRENT_USER"
-  echo "_CONTAINER_USER_HOME is $_CONTAINER_USER_HOME"
 
   if [[ -z $CURRENT_USER ]]; then
     echo 'eval "$(github-copilot-cli alias -- "$0")"' >> $_CONTAINER_USER_HOME/.zshrc
