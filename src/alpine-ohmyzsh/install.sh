74f6bc663c5701c08e7393c253ec32d16cf893c4
Ciro Lo Sapio <lscorionjs@gmail.com>
2024-09-09 16:46:26 +0000

    feat: Update alpine-ohmyzsh devcontainer feature to version 0.0.22 and modify plugin options
    - Updated the version of the alpine-ohmyzsh devcontainer feature to 0.0.22.
    - Modified the default plugins option in the devcontainer feature JSON file.
    - Removed the git alias-tips plugin from the default plugins list.
    - Added autoupdate plugin to the default plugins list.
    - Updated the install.sh script to clone the zsh-autosuggestions and zsh-syntax-highlighting plugins only if they are enabled.
    - Added support for the autoupdate plugin in the install.sh script.

--- src/alpine-ohmyzsh/install.sh
+++ src/alpine-ohmyzsh/install.sh
@@ -16,9 +16,19 @@ fi
 
 su -c "wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh -s" $_CONTAINER_USER
 
-git clone https://github.com/zsh-users/zsh-autosuggestions $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-sed -i 's/plugins=(git)/plugins=(\n  zsh-autosuggestions\n  zsh-syntax-highlighting\n)/' $_CONTAINER_USER_HOME/.zshrc
+sed -i 's/plugins=(git)/plugins=(\n)/' $_CONTAINER_USER_HOME/.zshrc
+
+if echo "$PLUGINS" | grep -w -q "zsh-autosuggestions"; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+fi
+
+if echo "$PLUGINS" | grep -w -q "zsh-syntax-highlighting"; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
+if echo "$PLUGINS" | grep -w -q "autoupdate"; then
+  git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/autoupdate
+fi
 
 if echo "$PLUGINS" | grep -w -q "autojump"; then
   git clone https://github.com/wting/autojump $_CONTAINER_USER_HOME/.oh-my-zsh/custom/plugins/autojump
