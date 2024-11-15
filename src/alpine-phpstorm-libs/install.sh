885cc2c9df079a4d95702a21b7651b37d692feab
Ciro Lo Sapio <lscorionjs@gmail.com>
2024-11-15 09:43:31 +0100

    Bump alpine-phpstorm-libs version to 0.0.2 and update install script dependencies

--- src/alpine-phpstorm-libs/install.sh
+++ src/alpine-phpstorm-libs/install.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Activating feature 'alpine-phpstorm-libs'"
 
-apk --no-cache add gcompat libxi libxrender libxtst
+apk --no-cache add curl unzip libxext libxrender libxtst libxi freetype procps gcompat
 
 echo 'Done!'
