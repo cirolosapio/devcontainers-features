4b77644c89478cc5310d3d22ee615d7e388355f2
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-01-08 12:07:12 +0100

    update mssql driver and tools to version 18.4.1.1

--- src/alpine-php-mssql/install.sh
+++ src/alpine-php-mssql/install.sh
@@ -7,12 +7,12 @@ echo "Activating feature 'alpine-php-mssql'"
 apk --no-cache add curl
 
 # Download the desired package(s)
-curl -O https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/msodbcsql18_18.3.3.1-1_amd64.apk
-curl -O https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/mssql-tools18_18.3.1.1-1_amd64.apk
+curl -O https://download.microsoft.com/download/7/6/d/76de322a-d860-4894-9945-f0cc5d6a45f8/msodbcsql18_18.4.1.1-1_amd64.apk
+curl -O https://download.microsoft.com/download/7/6/d/76de322a-d860-4894-9945-f0cc5d6a45f8/mssql-tools18_18.4.1.1-1_amd64.apk
 
 #Install the package(s)
-apk add --allow-untrusted msodbcsql18_18.3.3.1-1_amd64.apk
-apk add --allow-untrusted mssql-tools18_18.3.1.1-1_amd64.apk
+apk add --allow-untrusted msodbcsql18_18.4.1.1-1_amd.apk
+apk add --allow-untrusted mssql-tools18_18.4.1.1-1_amd.apk
 # TODO path like this is not updated
 PATH="$PATH:/opt/mssql-tools18/bin"
 
