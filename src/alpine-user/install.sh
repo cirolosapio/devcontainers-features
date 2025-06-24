9a15f9c3acb0f1c418fe6889d1865160ee9ff772
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-06-24 15:14:18 +0200

    add userFolders option to alpine-user feature

--- src/alpine-user/install.sh
+++ src/alpine-user/install.sh
@@ -6,4 +6,10 @@ echo "Activating feature 'alpine-user'"
 
 adduser -D $USER -u $UID
 
+for dir in $USERFOLDERS; do
+  if [ -d "$dir" ]; then
+    chown -R "$USER:$USER" "$dir"
+  fi
+done
+
 echo 'Done!'
