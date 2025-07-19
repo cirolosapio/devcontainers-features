dcf3ead01320c007a76a097b9b082d4c04ada898
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-07-19 16:16:47 +0200

    feat: add help zsh plugin

--- src/alpine-helm/install.sh
+++ src/alpine-helm/install.sh
@@ -6,4 +6,8 @@ echo "Activating feature 'alpine-helm'"
 
 apk --no-cache add helm
 
+if command -v zsh &> /dev/null; then
+  sed -i 's/^plugins=(/plugins=(\n  helm/g' $_CONTAINER_USER_HOME/.zshrc
+fi
+
 echo 'Done!'
