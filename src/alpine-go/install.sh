4790ca07aebf9232accbd250722e7a46a48e21b6
Ciro Lo Sapio <lscorionjs@gmail.com>
2025-07-19 16:15:48 +0200

    feat: add golang zsh plugin

--- src/alpine-go/install.sh
+++ src/alpine-go/install.sh
@@ -6,4 +6,8 @@ echo "Activating feature 'alpine-go'"
 
 apk --no-cache add go
 
+if command -v zsh &> /dev/null; then
+  sed -i 's/^plugins=(/plugins=(\n  golang/g' $_CONTAINER_USER_HOME/.zshrc
+fi
+
 echo 'Done!'
