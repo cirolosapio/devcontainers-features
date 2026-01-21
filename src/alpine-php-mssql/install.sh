6fafb2a3e7845c5cc1de9ab2b1fcc5bdc93f3081
Ciro Lo Sapio <lscorionjs@gmail.com>
2026-01-21 12:21:52 +0100

    update alpine-php-mssql package download links

--- src/alpine-php-mssql/install.sh
+++ src/alpine-php-mssql/install.sh
@@ -7,8 +7,8 @@ echo "Activating feature 'alpine-php-mssql'"
 apk --no-cache add curl
 
 # Download the desired package(s)
-curl -O https://download.microsoft.com/download/7/6/d/76de322a-d860-4894-9945-f0cc5d6a45f8/msodbcsql18_18.4.1.1-1_amd64.apk
-curl -O https://download.microsoft.com/download/7/6/d/76de322a-d860-4894-9945-f0cc5d6a45f8/mssql-tools18_18.4.1.1-1_amd64.apk
+curl -O https://download.microsoft.com/download/9dcab408-e0d4-4571-a81a-5a0951e3445f/msodbcsql18_18.6.1.1-1_amd64.apk
+curl -O https://download.microsoft.com/download/b60bb8b6-d398-4819-9950-2e30cf725fb0/mssql-tools18_18.6.1.1-1_amd64.apk
 
 #Install the package(s)
 apk add --allow-untrusted msodbcsql18_18.4.1.1-1_amd64.apk
