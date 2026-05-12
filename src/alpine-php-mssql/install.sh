062ecfabae122f4fb145a266d85b7d1df8427516
Ciro Lo Sapio <lscorionjs@gmail.com>
2026-05-12 10:14:25 +0200

    update alpine-php-mssql: update ODBC driver version

--- src/alpine-php-mssql/install.sh
+++ src/alpine-php-mssql/install.sh
@@ -7,12 +7,12 @@ echo "Activating feature 'alpine-php-mssql'"
 apk --no-cache add curl autoconf make g++
 
 # Download the desired package(s)
-curl -O https://download.microsoft.com/download/9dcab408-e0d4-4571-a81a-5a0951e3445f/msodbcsql18_18.6.1.1-1_amd64.apk
-curl -O https://download.microsoft.com/download/b60bb8b6-d398-4819-9950-2e30cf725fb0/mssql-tools18_18.6.1.1-1_amd64.apk
+curl -O https://download.microsoft.com/download/0b3d5518-b4a7-4a2b-afc7-7ee9e967f93c/msodbcsql18_18.6.2.1-1_amd64.apk
+curl -O https://download.microsoft.com/download/cad0d30f-b9b1-4765-a011-81d8a66c8b8d/mssql-tools18_18.6.2.1-1_amd64.apk
 
 #Install the package(s)
-apk add --allow-untrusted msodbcsql18_18.6.1.1-1_amd64.apk
-apk add --allow-untrusted mssql-tools18_18.6.1.1-1_amd64.apk
+apk add --allow-untrusted msodbcsql18_18.6.2.1-1_amd64.apk
+apk add --allow-untrusted mssql-tools18_18.6.2.1-1_amd64.apk
 # TODO path like this is not updated
 PATH="$PATH:/opt/mssql-tools18/bin"
 
